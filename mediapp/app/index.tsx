b697fff5625f3ce129049eab4c30330485e2db2e
Achbi <harshitbahety41@gmail.com>
2025-04-02 19:17:58 +0530

    Add Medcines Done

--- mediapp/app/index.tsx
+++ mediapp/app/index.tsx
@@ -1,78 +1,68 @@
-import {View , Text , StyleSheet ,Animated } from "react-native";
-import {Ionicons} from "@expo/vector-icons";
-import { useEffect,useRef } from "react";
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import AuthScreen from "./auth";
+import { useEffect, useRef } from "react";
 
-export default function SplashScreen(){
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    const scaleAnim = useRef(new Animated.Value(0.5)).current;
-    const router = useRouter(); 
+export default function SplashScreen() {
+  const router = useRouter();
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
 
-    useEffect(()=>{
-      Animated.parallel([
-         Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true,
-        }),
-        Animated.spring(scaleAnim,{
-            toValue: 1,
-            tension: 1000,
-            friction:2,
-            useNativeDriver: true,
-        }),
-      ]).start();
-      
-      const timer = setTimeout(() =>{
-         router.replace('/auth')
-      },2000)
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        tension: 10,
+        friction: 2,
+        useNativeDriver: true,
+      }),
+    ]).start();
 
-      return () => clearTimeout(timer);
+    const timer = setTimeout(() => {
+      router.replace("/home");
+    }, 2000);
 
+    return () => clearTimeout(timer);
+  }, []);
 
-    },[])
-
-    return(
-        <View style = {styles.container}>
-
-            <Animated.View style = {[
-                styles.iconContainer,
-                {
-                opacity:fadeAnim,
-                transform:[{scale:scaleAnim}],
-            },
-            ]}
-            >
-                <Ionicons name= "medical" size={100} color="white"/>
-            
-          
-            </Animated.View>
-            <Text>Med remind</Text>
-    
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.iconContainer,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      >
+        <Ionicons name="medical" size={100} color="white" />
+        <Text style={styles.appName}>MedRemind</Text>
+      </Animated.View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-       container:{
-        flex:1,
-        backgroundColor:'#4CAF50', 
-        alignItems:'center',
-        justifyContent:'center',
-       },
-       iconContainer:{
-        color:'white',
-        fontSize: 32,
-        fontWeight:"bold",
-        marginTop:20,
-        letterSpacing:1,
-       },
-       appName:{
-        color:'white',
-        fontSize:20,
-        fontWeight:"bold",
-        marginTop:20,
-        letterSpacing:1,
-       }
+  container: {
+    flex: 1,
+    backgroundColor: "#4CAF50",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer: {
+    alignItems: "center",
+  },
+  appName: {
+    color: "white",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 20,
+    letterSpacing: 1,
+  },
 });
